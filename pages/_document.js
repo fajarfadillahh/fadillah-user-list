1646dcbc7def2371c333c5c3ab3fbf04722443db
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-03 19:59:52 +0700

    setup project

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* favicon */}
+        <link rel="shortcut icon" href="/images/icon.svg" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/images/icon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
