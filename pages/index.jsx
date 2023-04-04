18d946268a18c5e647e9021d9167298e407622ca
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-04 17:45:52 +0700

    modified: fixed layouting on the Home & User page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,10 @@
 // import components
-import Layout from "@/components/Layout";
 import Hero from "@/components/Homepage/Hero";
 
 export default function Home() {
   return (
-    <Layout>
+    <>
       <Hero />
-    </Layout>
+    </>
   );
 }
--- pages/users.jsx
+++ pages/users.jsx
@@ -1,11 +1,10 @@
 // import components
-import Layout from "@/components/Layout";
 import UsersList from "@/components/Userspage/UsersList";
 
 export default function users() {
   return (
-    <Layout>
+    <>
       <UsersList />
-    </Layout>
+    </>
   );
 }
