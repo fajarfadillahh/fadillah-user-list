c9f1b1199b48d10635e2e74d4416fbb2d0d2041c
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-04 12:00:36 +0700

    create: Users page

---
+++ pages/users.jsx
@@ -0,0 +1,10 @@
+// import components
+import Layout from "@/components/Layout";
+
+export default function users() {
+  return (
+    <Layout>
+      <div className="pt-40">Users page</div>
+    </Layout>
+  );
+}
