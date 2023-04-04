57a056632d1719bf42d85f2a03532073f29e21a1
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-04 12:19:45 +0700

    create: UsersCard parts - Users page

---
+++ components/Userspage/UsersCard.jsx
@@ -0,0 +1,11 @@
+export default function UsersCard() {
+  return (
+    <div className="group relative w-[740px] cursor-pointer overflow-hidden rounded-lg bg-white shadow-md">
+      <div className="absolute left-0 top-0 h-full w-0 bg-pink-500 transition-all duration-100 group-hover:w-[6px]" />
+
+      <div className="flex h-[84px] items-center px-12">
+        <h1 className="text-[20px] font-bold text-gray-900">Leanne Graham</h1>
+      </div>
+    </div>
+  );
+}
