596e42f6608705b097c814ed9c6db01c016bddbc
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-05 15:27:55 +0700

    modified: add HandleThemeOnClick function on the Darkmode components

--- components/Darkmode.jsx
+++ components/Darkmode.jsx
@@ -14,10 +14,14 @@ export default function Darkmode() {
     return null;
   }
 
+  const handleThemeOnClick = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
     <div
       className="fixed bottom-12 right-12 inline-flex h-[42px] w-[42px] cursor-pointer items-center justify-center rounded-full bg-white text-[1.3rem] text-gray-900 shadow-lg transition hover:bg-pink-500 hover:text-white"
-      onClick={() => (theme === "light" ? setTheme("dark") : setTheme("light"))}
+      onClick={handleThemeOnClick}
     >
       {theme === "light" ? (
         <>
