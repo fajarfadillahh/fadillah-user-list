a62b639b9953a1b9388d31d0981e1d948efe1d7e
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-05 15:41:39 +0700

    modified: dark mode Hero parts - Home page

--- components/Darkmode.jsx
+++ components/Darkmode.jsx
@@ -20,7 +20,7 @@ export default function Darkmode() {
 
   return (
     <div
-      className="fixed bottom-12 right-12 inline-flex h-[42px] w-[42px] cursor-pointer items-center justify-center rounded-full bg-white text-[1.3rem] text-gray-900 shadow-lg transition hover:bg-pink-500 hover:text-white"
+      className="fixed bottom-12 right-12 inline-flex h-[42px] w-[42px] cursor-pointer items-center justify-center rounded-full bg-white text-[1.3rem] text-gray-900 shadow-lg transition hover:bg-pink-500 hover:text-white dark:bg-gray-800 dark:text-white dark:hover:bg-pink-500"
       onClick={handleThemeOnClick}
     >
       {theme === "light" ? (
