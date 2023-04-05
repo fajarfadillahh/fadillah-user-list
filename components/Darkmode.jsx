a624d2774ac7da5ca6c580c2b652b9d7ad770f71
fajarfadillahh <fajaragustian979@gmail.com>
2023-04-05 15:23:06 +0700

    modified: activated darkmmode feature

--- components/Darkmode.jsx
+++ components/Darkmode.jsx
@@ -1,9 +1,33 @@
-import { RiMoonClearLine } from "react-icons/ri";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { RiMoonClearLine, RiSunLine } from "react-icons/ri";
 
 export default function Darkmode() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
-    <div className="fixed bottom-12 right-12 inline-flex h-[42px] w-[42px] cursor-pointer items-center justify-center rounded-full bg-white text-[1.3rem] text-gray-900 shadow-lg transition hover:bg-pink-500 hover:text-white">
-      <RiMoonClearLine />
+    <div
+      className="fixed bottom-12 right-12 inline-flex h-[42px] w-[42px] cursor-pointer items-center justify-center rounded-full bg-white text-[1.3rem] text-gray-900 shadow-lg transition hover:bg-pink-500 hover:text-white"
+      onClick={() => (theme === "light" ? setTheme("dark") : setTheme("light"))}
+    >
+      {theme === "light" ? (
+        <>
+          <RiMoonClearLine />
+        </>
+      ) : (
+        <>
+          <RiSunLine />
+        </>
+      )}
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 // import components
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
